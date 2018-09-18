4ce47a01a5cb4f42f3130fa5879b43b56538b697
Matthew RienteR2H <matthewrienter2h@rv-clt-mriente.redventures.net>
2018-09-17 22:27:52 -0400

    complete but buggy

--- src/Products.js
+++ src/Products.js
@@ -11,6 +11,7 @@ export default class Products extends Component {
             admins: '',
             users: '',
             value: ''
+
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -33,81 +34,75 @@ export default class Products extends Component {
         event.preventDefault();
       }
 
+      sortByPriceHi(productA, productB) {
+        if (productA.price < productB.price) {
+            return -1;
+          }
+          if (productA.price > productB.price) {
+            return 1;
+          }
+          return 0;
+    }
+
+     sortByPriceLo(productA, productB) {
+        if (productB.price < productA.price) {
+            return -1;
+          }
+          if (productB.price > productA.price) {
+            return 1;
+          }
+          return 0;
+    }
+
+    // works but has a bug where it does not render on load and shows the wrong array
+    // (where the first card is always the mona lisa)
+    // at first when rendered, but when rendered another time it is fine??
+
       handleChange(event) {
+
+        let sortedProductsHi = this.state.products.slice().sort(this.sortByPriceHi);
+        let sortedProductsLo = this.state.products.slice().sort(this.sortByPriceLo);
+
             this.setState({value: event.target.value});
+            
+            if (this.state.value === "high") {
+                this.setState(() => {
+                    return {
+                     sortedProducts: sortedProductsHi
+                    }
+                });
+            }
+            if (this.state.value === "low") {
+                this.setState(() => {
+                    return {
+                     sortedProducts: sortedProductsLo
+                    }
+                   });
+            }
+            if (this.state.value === "none" || this.state.value === '') {
+                this.setState(() => {
+                    return {
+                     sortedProducts: this.state.products
+                    }
+                });
+            }
       }
 
     render() {
-        const products = this.state.products.slice();
+        const products = this.state.sortedProducts.slice();
         console.log(this.state.products)
-
-
-        function sortByPriceLo(productA, productB) {
-            if (productB.price < productA.price) {
-                return 1;
-              }
-              if (productB.price > productA.price) {
-                return -1;
-              }
-              // price must be equal
-              return 0;
-        }
-        
-        function sortByPriceHi(productA, productB) {
-            if (productA.price < productB.price) {
-                return 1;
-              }
-              if (productA.price > productB.price) {
-                return -1;
-              }
-              // price must be equal
-              return 0;
-        }
-
-        // set up SORT
-        const sortedProductsLo = products.slice().sort(sortByPriceLo);
-
-        const sortedProductsHi = products.slice().sort(sortByPriceHi);
-
-
-            // if (this.state.value === "high") {
-            //     this.setState(() => {
-            //         return {
-            //          sortedProducts: sortByPriceHi
-            //         }
-            //        });
-            // }
-            // if (this.state.value === "low") {
-            //     this.setState(() => {
-            //         return {
-            //          sortedProducts: sortByPriceHi
-            //         }
-            //        });
-            // }
-            // if (this.state.value === "none") {
-            //     this.setState(() => {
-            //         return {
-            //          sortedProducts: sortByPriceHi
-            //         }
-            //        });
-            // }
-
-        console.log("sorted lo", sortedProductsLo);
-        console.log("sorted Hi", sortedProductsHi);
         console.log("currentvalue", this.state.value)
-        // if statement to decide what comes first based on price
-        // render the results
 
         return (
             <div className='productCard'>
                 <label> Pick your price range:
-                    <select className='productCard__dropdown' onChange={this.handleChange}>
+                    <select className='productCard__dropdown' defaultValue="none" onChange={this.handleChange}>
                         <option value="none"> None </option>
                         <option value="high"> High </option>
                         <option value="low"> Low </option>
                     </select>
                 </label>
-                {this.state.sortedProducts.map( product =>
+                {products.map( product =>
                 <div className="productCard__container" key={product.productId}>
                     <img className="productCard__container__img" key={product.productImages.imageId} src={product.productImages[0].url} alt={product.productImages[0].caption} />
                     <h4 className="productCard__container__head">{product.author}, {product.title}</h4> 
